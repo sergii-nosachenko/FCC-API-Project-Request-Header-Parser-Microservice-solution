1b1cd755943d8684b220b021fe89263874883f6e
sergii-nosachenko <s.nosachenko@gmail.com>
2022-04-01 16:30:16 +0300

    App basic structure

---
+++ app.js
@@ -0,0 +1,37 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+
+const parser = require('./requestParser');
+
+const app = express();
+
+const port = process.env.PORT || 5000;
+
+// API MIDDLEWARE
+
+// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
+// so that your API is remotely testable by FCC
+
+app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
+
+app.use('/public', express.static('public'));
+
+app.use('/api/whoami', parser);
+
+// API ROUTES
+
+// Index route, shows index.html page
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/index.html');
+});
+
+// API GET route
+app.get('/api/whoami', (req, res) => {
+  res.json(req.json);
+});
+
+// SERVER RUN
+app.listen(port, () => {
+  console.log('Server running on port: ', port);
+});
